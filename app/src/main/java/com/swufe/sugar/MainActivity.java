0baaa88e2bd1d8cb348f42d6f82a80c6b084b815
Cissbeet <1152451513@qq.com>
2020-06-10 17:26:21 +0800

    欢迎页面及跳转

--- app/src/main/java/com/swufe/sugar/MainActivity.java
+++ app/src/main/java/com/swufe/sugar/MainActivity.java
@@ -1,14 +1,24 @@
 package com.swufe.sugar;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
+    String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onclick(View btn){
+        //跳转页面，使用intent对象
+        Log.i(TAG, "onclick: ");
+        Intent pagechange = new Intent(this, PageChange.class);
+        startActivity(pagechange);
+
     }
 }
---
+++ app/src/main/java/com/swufe/sugar/PageChange.java
@@ -0,0 +1,18 @@
+package com.swufe.sugar;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PageChange extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_change);
+        //使用MyPageAdapter连接viewpage和fragment
+
+
+
+    }
+}
